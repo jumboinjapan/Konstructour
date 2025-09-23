84acd15adc6f49bbdb8566b7dad44e774460fcfd
Eduard Revidovich <jumbo@Eduards-Mac-mini.local>
2025-09-24 01:44:49 +0900

    chore: sync data and push

---
+++ api/config.php
@@ -0,0 +1,14 @@
+<?php
+return array (
+  'airtable_registry' => 
+  array (
+    'baseId' => 'apppwhjFN82N9zNqm',
+    'tables' => 
+    array (
+      'region' => 
+      array (
+        'tableId' => 'tblbSajWkzI8X7M4U',
+      ),
+    ),
+  ),
+);
